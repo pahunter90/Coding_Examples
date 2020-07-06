cb3be34317c1a87363ed4b960db83da694858666
Pierce Hunter <phunter@uoregon.edu>
2020-07-06 09:59:40 -0700

    two TF tutorials

---
+++ NumPy/numpy_100.py
@@ -0,0 +1,24 @@
+# 1. Import the numpy package under the name np
+
+import numpy as np
+
+
+# 2. Print the numpy version and the configuration 
+
+print(np.__version__)
+print(np.show_config())
+
+
+# 3. Create a null vector of size 10
+
+v = np.zeros(10)
+
+
+# 4. How to find the memory size of any array
+
+print(v.__sizeof__())
+
+
+# 5. How to get the documentation of the numpy add function from the command line?
+
+
